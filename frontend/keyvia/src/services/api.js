7196178e5e9f201265334c7ee9a445a8e9628155
Abhishekkjainn <jainabhishek1904@gmail.com>
2025-09-21 20:46:51 +0530

    Fixed Analytics page Error

--- frontend/keyvia/src/services/api.js
+++ frontend/keyvia/src/services/api.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = 'http://localhost:8080'; // Or use an environment variable
+// const API_BASE_URL = 'http://localhost:8080/'; // Or use an environment variable
 const API_BASE_URL = 'https://keyvia-backend.onrender.com';
 import { sniffMimeFromBytes } from '../utils/mimeSniff';
 
