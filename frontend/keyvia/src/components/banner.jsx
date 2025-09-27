0482c53b8c797e6c8a6ec2cb1a842e7be1caeae2
Abhishekkjainn <jainabhishek1904@gmail.com>
2025-09-27 16:25:59 +0530

    Added Doc link

--- frontend/keyvia/src/components/banner.jsx
+++ frontend/keyvia/src/components/banner.jsx
@@ -8,7 +8,7 @@ export default function Banner(){
                     <h1 className="banner-title">Secure Your Digital World</h1>
                     <p className="banner-desc">A next-gen vault for your files with intelligent deduplication, blazing-fast search, and sharing that feels effortless.</p>
                     <div className="banner-actions">
-                        {user ? (<><Link to={'/login'} className="primbutton1">Go to Dashboard</Link> <Link to={'/documentation'} className="button2">Documentation</Link></>) : (<>
+                        {user ? (<><Link to={'/login'} className="primbutton1">Go to Dashboard</Link> <a href='https://documenter.getpostman.com/view/39857190/2sB3HtFwWi' className="button2">Documentation</a></>) : (<>
             <Link to={'/login'} className="button1">Sign In</Link>
             <Link to={'/register'} className="button2">Register Now</Link></>) }
                     </div>
--- frontend/keyvia/src/pages/herosection.jsx
+++ frontend/keyvia/src/pages/herosection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection(){
         </div>
         <div className="subtagline">Designed for efficiency and trust, this next-gen vault helps you upload, search, and share with confidence, while saving space through intelligent deduplication and ensuring every file remains secure and accessible</div>
         <div className="buttonsection">
-            {user ? (<><Link to={'/login'} className="primbutton1">Go to Dashboard</Link> <Link to={'/documentation'} className="button2">Documentation</Link></>) : (<>
+            {user ? (<><Link to={'/login'} className="primbutton1">Go to Dashboard</Link> <a href='https://documenter.getpostman.com/view/39857190/2sB3HtFwWi' className="button2">Documentation</a></>) : (<>
             <Link to={'/login'} className="button1">Sign In</Link>
             <Link to={'/register'} className="button2">Register Now</Link></>) }
             
